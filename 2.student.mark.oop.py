a42a785e8cf0e2add22c1fe9aacaba1f687a441e
songuyen24 <nguyensonnty2@gmail.com>
2021-04-25 21:16:51 +0700

    labwork2

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,28 +1,53 @@
+def studentNumber():
+    global studentNumber
+    studentNumber = int(input("number of student :"))
+def Course():
+    global Course
+    Course = int(input("number of course : "))
+def StudentMark():
+    global StudentMark
+    StudentMark = int(input("student's mark : "))
+    
+    
 class StudentInformation():
     def __init__(self, ID, name, Dob):
         self.ID = ID
         self.name = name
         self.Dob = dob
-    def StudentInfo(self):
+    def getStudentInfo(self):
         print("ID : " + self.ID)
         print("Name : " + self.name)
         print("Dob : " + self.Dob)
+    def setStudentInfo(self, ID, name, Dob):
+        self.ID = ID
+        self.name = name
+        self.Dob = Dob
+        
 class Course():
     def __init__(self, name, ID, number):
         self.name = name
         self.ID = ID
         self.number = number
-    def CourseInfo(self):
+    def getCourseInfo(self):
         print("Name : " + self.name)
         print("ID : " + self.ID)
         print("number :" + self.number)
+    def setCourseInfo(self, name, ID, number):
+        self.name = name
+        self.ID = ID
+        self.number = number
+    
 class StudentMark():
     
     def __init__(self, studentName, Mark, Course):
         self.studentName = studentName
         self.Mark = Mark
         self.course = course
-    def MarkInfo():
+    def getMarkInfo():
         print("studentName :" + self.studentName)
         print("Mark :" + self.Mark)
         print("course :" + self.course)
+    def setMarkInfo(self, studentName, Mark, Course):
+        self.studentName = studentName
+        self.Mark = Mark
+        self.course = course
